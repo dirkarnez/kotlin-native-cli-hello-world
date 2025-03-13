98e6707083a545023df06ea3221ccd585d368d0e
Dirk Arnez <smalldirkalex@gmail.com>
2025-03-13 22:46:53 +0800

    - upload files

---
+++ local-build.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set PATH=^
+%USERPROFILE%\Downloads\kotlin-native-prebuilt-windows-x86_64-2.1.10\bin;^
+%USERPROFILE%\Downloads\OpenJDK8U-jdk_x64_windows_hotspot_8u382b05\jdk8u382-b05\bin
+
+kotlinc-native.bat main.kt -o main
+pause
