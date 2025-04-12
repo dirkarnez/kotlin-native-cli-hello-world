b7e92cebf42f435568bb8d44cead206cbeec96ec
Dirk Arnez <smalldirkalex@gmail.com>
2025-04-12 17:27:47 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set PATH=^
-%USERPROFILE%\Downloads\kotlin-native-prebuilt-windows-x86_64-2.1.10\bin;^
+%USERPROFILE%\Downloads\kotlin-native-prebuilt-windows-x86_64-2.1.10\kotlin-native-prebuilt-windows-x86_64-2.1.10\bin;^
 %USERPROFILE%\Downloads\OpenJDK8U-jdk_x64_windows_hotspot_8u382b05\jdk8u382-b05\bin
 
 kotlinc-native.bat main.kt -o main
